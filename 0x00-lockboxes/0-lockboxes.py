bdf478e56381c282f9b8d32d322a560d1b8a97fc
Brent Muha <brent.muha@gmail.com>
2020-07-16 16:11:15 -0700

    Add lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+
+Write a method that determines if all the boxes can be opened.
+Box 0 is unlocked.
+'''
+
+
+def canUnlockAll(boxes):
+    '''
+    canUnlockAll - Determine if all the boxes can be opened
+    @boxes: List of lists containing the keys
+
+    Return: True if all the boxes can be opened, False otherwise
+    '''
+    open = openBox(boxes, 0, {0})
+    return len(open) == len(boxes)
+
+
+def openBox(boxes, box, open):
+    '''
+    openBox - Open the specified box
+    @boxes: List of lists containing the keys
+    @box: The current box to open
+    @open: Set containing the boxes that are currently open
+
+    Return: Set containing all the boxes that can be opened
+    '''
+    for key in boxes[box]:
+        if key not in open:
+            open.add(key)
+            openBox(boxes, key, open)
+    return open
