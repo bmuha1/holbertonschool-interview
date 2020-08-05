80614f989189a04430933c51cd53c6317d26b434
Brent Muha <brent.muha@gmail.com>
2020-08-05 10:31:53 -0700

    Add min operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n, write
+a method that calculates the fewest number of operations needed to result in
+exactly n H characters in the file.
+'''
+
+
+def minOperations(n):
+    '''
+    minOperations - Return the fewest number of operations needed
+    @n: The number of H characters needed
+
+    Return: The fewest number of operations needed, or 0
+    '''
+    if n < 2:
+        return 0
+    if n < 6:
+        return n
+    min = {}
+    for i in range(2, 6):
+        min[i] = i
+    for i in range(n - 1, 1, -1):
+        if n % i == 0:
+            if not min.get(i):
+                min[i] = minOperations(i)
+            return min[i] + n // i
+    return n
