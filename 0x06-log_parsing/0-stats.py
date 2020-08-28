66f4fcec4d99f453df85f53dc0da6508eb3967f8
Brent Muha <brent.muha@gmail.com>
2020-08-28 15:28:01 -0700

    Add log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''
+Write a script that reads stdin line by line and computes metrics:
+
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+    (see input format above)
+    Number of lines by status code:
+        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+        if a status code doesn’t appear, don’t print anything for this
+        status code
+        format: <status code>: <number>
+        status codes should be printed in ascending order
+'''
+import sys
+
+
+def print_status(size, status):
+    '''
+    Print the status
+    '''
+    print('File size: {}'.format(size))
+    for key, value in sorted(status.items()):
+        print('{}: {}'.format(key, value))
+
+
+if __name__ == '__main__':
+    size, count = 0, 0
+    status = {}
+    try:
+        for line in sys.stdin:
+            args = line.split()
+            if args[7] in status:
+                status[args[7]] += 1
+            else:
+                status[args[7]] = 1
+            size += int(args[8])
+            count += 1
+            if not count % 10:
+                print_status(size, status)
+    finally:
+        print_status(size, status)
