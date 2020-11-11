1c34580aee94296e1d5b1635a2cbae409757dc86
Brent Muha <brent.muha@gmail.com>
2020-11-10 16:07:46 -0800

    Fix betty

--- 0x0E-linear_skip/1-main.c
+++ 0x0E-linear_skip/1-main.c
@@ -16,8 +16,8 @@ int main(void)
 {
 	skiplist_t *list, *res;
 	int array[] = {
-        0, 1, 2, 3, 4, 7, 18, 18, 18, 19, 23, 53, 61, 62, 76, 99
-    };
+		0, 1, 2, 3, 4, 7, 18, 18, 18, 19, 23, 53, 61, 62, 76, 99
+	};
 	size_t size = sizeof(array) / sizeof(array[0]);
 
 	list = create_skiplist(array, size);
